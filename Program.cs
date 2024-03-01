99967fa8c50d19ad8172efef35ae565a405dfedb
Thomas Kibby <htkibby@gmail.com>
2024-02-29 18:09:37 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string filePath = System.IO.Path.GetFullPath("Dimensions.txt");
+StreamReader dimensions = new(filePath);
+
+string line = dimensions.ReadLine();
+int totalSquareFeet = 0;
+
+while (line != null)
+{
+    string[] subs = line.Split('x', 3);
+    int l = Int32.Parse(subs[0]);
+    int w = Int32.Parse(subs[1]);
+    int h = Int32.Parse(subs[2]);
+
+    List<int> nums = [l, w, h];
+    nums.Remove(nums.Max());
+    int area = nums[0] * nums[1];
+
+    int result = (2 * l * w) + (2 * w * h) + (2 * h * l) + area;
+    totalSquareFeet += result;
+
+    line = dimensions.ReadLine();
+} 
+
+Console.WriteLine(totalSquareFeet);
+
